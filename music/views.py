044a5413e79b53828641261cb49d37bcff14bafd
Amit <ak7310518@gmail.com>
2019-03-04 16:43:35 +0530

    this is 4th commit

--- music/views.py
+++ music/views.py
@@ -123,7 +123,7 @@ def favorite_album(request, album_id):
 
 
 def index(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated():
         return render(request, 'music/login.html')
     else:
         albums = Album.objects.filter(user=request.user)
@@ -192,7 +192,7 @@ def register(request):
 
 
 def songs(request, filter_by):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated():
         return render(request, 'music/login.html')
     else:
         try:
